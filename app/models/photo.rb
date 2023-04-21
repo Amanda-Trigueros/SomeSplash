64ed5bb05baafd4f163f47f9b692d69861dc10cf
Amanda Trigueros <amandatrigueros01@gmail.com>
2023-04-21 15:53:01 -0600

    add: comment_count

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :category, counter_cache: true, optional: true
   has_many :comments, as: :commentable, dependent: :destroy
 
-  validates :title, presence: true 
-  validates :description, length: { in: 20..100 }, allow_blank: true
+  validates :title, presence: true
+  validates :description, length: { in: 1..100 }, allow_blank: true
 end
