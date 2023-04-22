7f55a6daeedde7719f17609e990983c8638984b8
Italo Flores <ifloressar15>
2023-04-21 22:51:59 -0500

    HTML & CSS Initial Kickoff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :photos
-  resources :categories
+    resources :photos
+    resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
